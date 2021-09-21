d426e7a8307eaac67e2b494e8996640a1ddee9e5
balintbiro <biro.balint.szie@gmail.com>
2021-09-21 13:20:38 +0200

    get genome version and sequences

--- code/sequence_downloading.py
+++ code/sequence_downloading.py
@@ -3,11 +3,39 @@ import os
 import pandas as pd
 import urllib.request   
 
-#create repository for every organisms
+#function for creating repository for every organisms
 def mkdir(organism_name):
     parent_dir='../data/'
     path=os.path.join(parent_dir+organism_name)
-    os.mkdir(path)
+    if os.path.exists(path):
+        pass
+    else:
+        os.mkdir(path)
     
 organisms=pd.Series(['Oryctolagus_cuniculus'])
 organisms.apply(mkdir)
+
+#function for getting the latest genome version and required sequences
+def get_sequences(organism_name):
+    #get the latest genome version
+    output_dir=os.path.join(f'../data/{organism_name}/')
+    filename='CHECKSUMS'
+    output_filepath=output_dir+filename
+    checksums_url=f'http://ftp.ensembl.org/pub/release-104/fasta/{organism_name.lower()}/dna/{filename}'
+    urllib.request.urlretrieve(checksums_url, output_filepath)
+    genome_version=''
+    #get the sequences based on the previously acquired genome version
+    with open(f'../data/{organism_name}/CHECKSUMS')as infile:
+        content=pd.Series(infile.readlines())
+        mask=content.apply(lambda line: organism_name in line)
+        genome_version=list(content[mask].apply(lambda line: line.split(organism_name+'.')[1].split('.dna')[0]))[0]
+    genome_name=f'{organism_name}.{genome_version}.dna.toplevel.fa.gz'
+    mt_name=f'{organism_name}.{genome_version}.dna.chromosome.MT.fa'
+    genome_url=f'http://ftp.ensembl.org/pub/release-104/fasta/{organism_name.lower()}/dna/{genome_name}'
+    mt_url=f'http://ftp.ensembl.org/pub/release-104/fasta/{organism_name.lower()}/dna/{mt_name}'
+    genome_filepath=output_dir+genome_name
+    mt_filepath=output_dir+mt_name
+    #urllib.request.urlretrieve(genome_url, genome_filepath)
+    urllib.request.urlretrieve(mt_url, mt_filepath)
+    
+organisms.apply(get_sequences)
