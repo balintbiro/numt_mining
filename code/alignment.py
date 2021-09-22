732ea1f47596fa8cc93ed603b758505929e5f92e
balintbiro <biro.balint.szie@gmail.com>
2021-09-22 18:34:39 +0200

    function for getting significant alignments

--- code/alignment.py
+++ code/alignment.py
@@ -36,3 +36,27 @@ def align_sequences(organism_name):
     call('lastal -r1 -q1 -a7 -b1 db d_mt.fa > d_mt_alignment.fa', shell=True)#align the genome and double mt dna into a file called d_mt_alignment.fa
     
 organisms.apply(align_r_mt)
+
+#function for getting the e-value threshold and mask the significant alignments based on that value
+def signifcant_alignments(organism_name):
+    home_dir=os.path.join(f'../data/{organism_name}/')
+    e_values=[]
+    with open(home_dir+'r_mt_alignment.fa')as infile:
+        content=pd.Series(infile.readlines())
+        mask=content.apply(lambda line: 'EG2' in line)
+        content[mask].apply(lambda line: e_values.append(float(line.rsplit()[3].split('=')[1])))
+    e_values.sort()
+    e_threshold=e_values[0]
+    with open(home_dir+'d_mt_alignment.fa')as infile, open(home_dir+'signifcant_alignments.fa','w')as outfile:
+        content=infile.readlines()
+        for index, line in enumerate(content):
+            e_value=float(line.rsplit()[3].split('=')[1])
+            g_sequence = content[index + 1]
+            mt_sequence = content[index + 2]
+            if e_value < e_threshold:
+                outfile.write(line)
+                outfile.write(g_sequence)
+                outfile.write(mt_sequence)
+                outfile.write('\n')
+                
+organism.apply(signifcant_alignments)
