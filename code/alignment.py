f3a02e47364d31dedb8a7a999115d1da882712dd
balintbiro <biro.balint.szie@gmail.com>
2021-09-22 16:35:47 +0200

    function for align mt dna and genome

--- code/alignment.py
+++ code/alignment.py
@@ -6,11 +6,11 @@ from subprocess import call
 #function for writing reversed mt sequence
 def create_r_mt(organism_name):
     organism_dir=os.path.join(f'../data/{organism_name}/')
-    mt_filename=f'{organism_name}.mt.fa'
+    mt_filename='mt.fa'
     mt_path=organism_dir+mt_filename
     header=''
     mt_seq=''
-    with open(mt_path)as infile, open(organism_dir+'r_mt_seq.fa','w')as outfile:
+    with open(mt_path)as infile, open(organism_dir+'r_mt.fa','w')as outfile:
         for line in infile:
             if '>' in line:
                 header=line
@@ -19,11 +19,16 @@ def create_r_mt(organism_name):
         r_mt_seq=mt_seq[::-1]
         outfile.write(header)
         outfile.write(r_mt_seq)
-        
-#function for creating LASTAL database for each organisms and align reversed mt dna with nuclear genome
-def align_r_mt(organism_name):
-    
 
 organisms=pd.Series(['Oryctolagus_cuniculus'])
 
+
 organisms.apply(create_r_mt)
+
+#function for creating LASTAL database for each organisms and align reversed mt dna with nuclear genome
+def align_r_mt(organism_name):
+    call('cd ../data/"${organism_name}"')#change the working directory
+    call('lastdb db genome.fa', shell=True)#building database
+    call('lastal db r_mt.fa > r_mt_alignment.fa')#align the genome and reversed mt dna into a file called r_mt_alignment.fa
+    
+organisms.apply(align_r_mt)
