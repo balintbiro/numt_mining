0a6c68b0270d8a5f1984c278264ba3ebe9184b57
balintbiro <biro.balint.szie@gmail.com>
2021-11-30 19:06:33 +0100

    scripts for delete alternative contigs

---
+++ code/genome_processing.bash
@@ -0,0 +1,6 @@
+grep -E '>' genome.fa >> headers.txt #get all the headers from the genome
+grep -E '>*REF' headers.txt >> ref_headers.txt #get the reference headers
+
+#if the size of headers != size of ref_headers, get the genome without alternative contigs!
+awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < genome.fa > processed_genome.fa #transform genome to one liner form
+grep -f ref_headers.txt processed_genome.fa >> new_genome.fa #get the reference sequences
