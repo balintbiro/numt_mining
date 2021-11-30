acd34a644e1dc6177f76b723ccf2088a5acf0015
balintbiro <biro.balint.szie@gmail.com>
2021-11-30 19:15:35 +0100

    script for process alt contigs
    Some genome (eg.: Human) contains alternative contigs. This phenomenon cannot be interpreted by LASTAL so a program was needed to process the genome and the reference sequences without alternative contigs.

--- code/genome_processing.bash
+++ code/genome_processing.bash
@@ -3,4 +3,4 @@ grep -E '>*REF' headers.txt >> ref_headers.txt #get the reference headers
 
 #if the size of headers != size of ref_headers, get the genome without alternative contigs!
 awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < genome.fa > processed_genome.fa #transform genome to one liner form
-grep -f ref_headers.txt processed_genome.fa >> new_genome.fa #get the reference sequences
+grep -f ref_headers.txt -A1 processed_genome.fa >> new_genome.fa #get the reference sequences
