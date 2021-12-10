dced44fd92272b4937474c2e2c36b5ac5ec33306
balintbiro <biro.balint.szie@gmail.com>
2021-12-10 10:33:07 +0100

    use """ for LINUX commands

--- code/2_genome_processing.py
+++ code/2_genome_processing.py
@@ -20,13 +20,13 @@ organisms=pd.Series(list(set(default_organisms)-set(problematic_organisms)))
 #define function for genome processing
 def process_genome(organism_name):
     os.chdir(f'../data/{organism_name}/')#change the working directory
-    call('grep -E '>' genome.fa >> headers.txt', shell=True)#get all the headers from the genome
-    call('grep -E '>*REF' headers.txt >> ref_headers.txt', shell=True)#get the reference headers
+    call("""grep -E '>' genome.fa >> headers.txt""", shell=True)#get all the headers from the genome
+    call("""grep -E '>*REF' headers.txt >> ref_headers.txt""", shell=True)#get the reference headers
     if filecmp.cmp('headers.txt','ref_headers.txt')==False:#that means that there are alternative headers
         os.rename('genome.fa','alt_genome.fa')#reanem the original genome.fa file to alt_genome.fa which contains the alternative sequences
-        call('awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < alt_genome.fa > processed_genome.fa', shell=True) #transform alt_genome to one liner form
-        call('grep -f ref_headers.txt -A1 processed_genome.fa >> genome.fa', shell=True)#get the reference sequences based on the reference headers in the ref_headers.txt file
-        call('sed -i '/^-/d' genome.fa', shell=True)##delete the line starting with '-' sign which is is introduced by grep command
+        call("""awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < alt_genome.fa > processed_genome.fa""", shell=True) #transform alt_genome to one liner form
+        call("""grep -f ref_headers.txt -A1 processed_genome.fa >> genome.fa""", shell=True)#get the reference sequences based on the reference headers in the ref_headers.txt file
+        call("""sed -i '/^-/d' genome.fa""", shell=True)##delete the line starting with '-' sign which is is introduced by grep command
     os.chdir('../../code/')#change back to the default 'code/' directory
     
 organisms.apply(align_sequences)
