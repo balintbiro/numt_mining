a1d379e34da97daa6ddf032703c7a6ed8ae2357a
balintbiro <biro.balint.szie@gmail.com>
2021-12-13 14:17:05 +0100

    create a file instead of appending to an old one

--- code/2_genome_processing.py
+++ code/2_genome_processing.py
@@ -21,8 +21,8 @@ organisms=pd.Series(list(set(default_organisms)-set(problematic_organisms)))
 #define function for genome processing
 def process_genome(organism_name):
     os.chdir(f'../data/{organism_name}/')#change the working directory
-    call("""grep -E '>' genome.fa >> headers.txt""", shell=True)#get all the headers from the genome
-    call("""grep -E '>*REF' headers.txt >> ref_headers.txt""", shell=True)#get the reference headers
+    call("""grep -E '>' genome.fa > headers.txt""", shell=True)#get all the headers from the genome
+    call("""grep -E '>*REF' headers.txt > ref_headers.txt""", shell=True)#get the reference headers
     if filecmp.cmp('headers.txt','ref_headers.txt')==False:#that means that there are alternative headers
         os.rename('genome.fa','alt_genome.fa')#rename the original genome.fa file to alt_genome.fa which contains the alternative sequences
         
