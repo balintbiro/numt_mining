10464956f09716476c79a0a74380b0481326bfe2
balintbiro <biro.balint.szie@gmail.com>
2021-12-13 13:51:06 +0100

    change one BASH part to Python
    Chnage BASH awk part to Python since subprocess.call() cannot interpret different quotation marks in BASH commands. The original BASH command was: r""awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < alt_genome.fa > processed_genome.fa""

--- code/2_genome_processing.py
+++ code/2_genome_processing.py
@@ -24,8 +24,16 @@ def process_genome(organism_name):
     call("""grep -E '>' genome.fa >> headers.txt""", shell=True)#get all the headers from the genome
     call("""grep -E '>*REF' headers.txt >> ref_headers.txt""", shell=True)#get the reference headers
     if filecmp.cmp('headers.txt','ref_headers.txt')==False:#that means that there are alternative headers
-        os.rename('genome.fa','alt_genome.fa')#reanem the original genome.fa file to alt_genome.fa which contains the alternative sequences
-        call("""awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < alt_genome.fa > processed_genome.fa""", shell=True) #transform alt_genome to one liner form
+        os.rename('genome.fa','alt_genome.fa')#rename the original genome.fa file to alt_genome.fa which contains the alternative sequences
+        
+        with open('alt_genome.fa')as infile, open('alt_genome.fa','w')as outfile: #transform alt_genome to one liner form
+            for line in alt_genome:
+                if '>' in line:
+                    outfile.write('\n')
+                    outfile.write(line)
+                else:
+                    outfile.write(line[:-1])
+        
         call("""grep -f ref_headers.txt -A1 processed_genome.fa >> genome.fa""", shell=True)#get the reference sequences based on the reference headers in the ref_headers.txt file
         call("""sed -i '/^-/d' genome.fa""", shell=True)##delete the line starting with '-' sign which is is introduced by grep command
     os.chdir('../../code/')#change back to the default 'code/' directory
