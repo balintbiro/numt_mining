64986ada990d25e4d2a346da20176ecf39b201da
balintbiro <biro.balint.szie@gmail.com>
2022-01-05 14:12:55 +0100

    exception handling

--- code/intragenic_numts.R
+++ code/intragenic_numts.R
@@ -3,6 +3,9 @@
 #load the package
 library(biomaRt)
 
+#load the csv file
+data<-read.csv('Z:/balint/numt/ensembl_tester.csv')
+
 #define function for creating ENsembl database and dataset
 create_ensembl_db_ds<-function(mouse_strain){
   strain<-strsplit(mouse_strain,'_',FALSE)
@@ -17,73 +20,64 @@ ensembl_db<-sapply(unique(data['organism_name'][[1]]),create_ensembl_db_ds)
 
 #define function for querying Ensembl
 ensembl_query<-function(row){
-  strain<-strsplit(as.character(row['organism_name']),'_',FALSE)[[1]][3]
+  
+  mouse_strain<-row['organism_name']
+  ensembl<-ensembl_db[mouse_strain][[1]]
   chr_name<-row['g_id']
   numt_start<-as.numeric(row['g_start'])
   numt_end<-numt_start+as.numeric(row['g_length'])
   
-  #check the avaialable databases
-  #listEnsembl()
-  #check the avaiable datasets
-  #listDatasets(ensembl)
-  ensembl<-useEnsembl(biomart='mouse_strains',#select the desired database
-                      dataset=paste('mm',strain,'_gene_ensembl',sep=''),#select the desired dataset
-                      mirror='asia')#select the mirror, options -->useast,uswest,asia,www
-  
-  #check the available filters and attributes
-  #listFilters(ensembl)
-  #listAttributes(ensembl)
   informations<-getBM(attributes=c('external_gene_name','uniprotswissprot'),
-               filters=c('chromosome_name','start','end'),
-               values=list(chromosome=chr_name,start=numt_start,end=numt_end),
+               filters=c('chromosome_name','start','end'),#all available listFilters(ensembl)
+               values=list(chromosome=chr_name,start=numt_start,end=numt_end),#all available listAttributes(ensembl)
                mart=ensembl)
-  return(as.vector(unname(informations)))
+  if (is.logical(informations[[1]])){
+    gene_names<<-append(gene_names,NA)
+    uniprot_ids<<-append(uniprot_ids,NA)
+  }
+  else{
+    gene_names<<-append(gene_names,list(informations['external_gene_name'][[1]]))
+    uniprot_ids<<-append(uniprot_ids,list(informations['uniprotswissprot'][[1]]))
+  }
 }
 
-#load the csv file
-data<-read.csv('Z:/balint/numt/ensembl_tester.csv')
+#define function for querying Ensembl
+ensembl_query<-function(row){
+  out<-tryCatch(
+    
+    #try branch
+    {
+      mouse_strain<-row['organism_name']
+      ensembl<-ensembl_db[mouse_strain][[1]]
+      chr_name<-row['g_id']
+      numt_start<-as.numeric(row['g_start'])
+      numt_end<-numt_start+as.numeric(row['g_length'])
+      
+      informations<-getBM(attributes=c('external_gene_name','uniprotswissprot'),
+                          filters=c('chromosome_name','start','end'),#all available listFilters(ensembl)
+                          values=list(chromosome=chr_name,start=numt_start,end=numt_end),#all available listAttributes(ensembl)
+                          mart=ensembl)
+      if (is.logical(informations[[1]])){
+        gene_names<<-append(gene_names,NA)
+        uniprot_ids<<-append(uniprot_ids,NA)
+      }
+      else{
+        gene_names<<-append(gene_names,list(informations['external_gene_name'][[1]]))
+        uniprot_ids<<-append(uniprot_ids,list(informations['uniprotswissprot'][[1]]))
+      }
+    },
+    
+    #except branch
+    error=function(row){
+      gene_names<<-append(gene_names,NA)
+      uniprot_ids<<-append(uniprot_ids,NA)
+    }
+  )
+  
+}
 
-para<-apply(data,1,ensembl_query)
-write.csv(para,'Z:/balint/numt/proba_output.csv')
-ensembl <- useEnsembl(biomart='mouse_strains',#select the desired database
-                      dataset='mm129s1svimj_gene_ensembl',#select the desired dataset
-                      mirror='asia')#select the mirror, options -->useast,uswest,asia,www
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+gene_names<-c()
+uniprot_ids<-c()
+proba<-apply(data,1,ensembl_query)
 
 
