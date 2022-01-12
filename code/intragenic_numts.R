41de6b315d23de396d9b7c914f3e9cb36eadf0e8
balintbiro <biro.balint.szie@gmail.com>
2022-01-12 13:31:43 +0100

    intragenic numts data prep and biomaRt

--- code/intragenic_numts.R
+++ code/intragenic_numts.R
@@ -3,80 +3,20 @@
 #load the package
 library(biomaRt)
 
-#load the csv file
-data<-read.csv('Z:/balint/numt/ensembl_tester.csv')
+ensembl_db<-useEnsembl(biomart='mouse_strains',#select the desired database, all available listEnsembl()
+                    dataset='mmaj_gene_ensembl',#select the desired dataset, all available listDatasets(ensembl)
+                    mirror='useast')#select the mirror, options -->useast,uswest,asia,www
 
-#define function for creating ENsembl database and dataset
-create_ensembl_db_ds<-function(mouse_strain){
-  strain<-strsplit(mouse_strain,'_',FALSE)
-  ensembl_strain<-paste('mm',strain[[1]][3],'_gene_ensembl',sep='')
-  ensembl<-useEnsembl(biomart='mouse_strains',#select the desired database, all available listEnsembl()
-                      dataset=ensembl_strain,#select the desired dataset, all available listDatasets(ensembl)
-                      mirror='asia')#select the mirror, options -->useast,uswest,asia,www
+genomic_regions<-scan('Z:/balint/numt/mice_strains_numt/genomic_regions/mmaj_gene_ensembl.txt',character())
+
+ensembl_query<-function(genomic_region){
+  ensembl<-getBM(attributes=c('external_gene_name'),
+                 filters=c('chromosomal_region'),#all available listFilters(ensembl)
+                 values=list(genomic_region),#all available listAttributes(ensembl)
+                 mart=ensembl_db)
   return(ensembl)
 }
 
-ensembl_db<-sapply(unique(data['organism_name'][[1]]),create_ensembl_db_ds)
-
-#define function for querying Ensembl
-ensembl_query<-function(row){
-  
-  mouse_strain<-row['organism_name']
-  ensembl<-ensembl_db[mouse_strain][[1]]
-  chr_name<-row['g_id']
-  numt_start<-as.numeric(row['g_start'])
-  numt_end<-numt_start+as.numeric(row['g_length'])
-  
-  informations<-getBM(attributes=c('external_gene_name','uniprotswissprot'),
-               filters=c('chromosome_name','start','end'),#all available listFilters(ensembl)
-               values=list(chromosome=chr_name,start=numt_start,end=numt_end),#all available listAttributes(ensembl)
-               mart=ensembl)
-  if (is.logical(informations[[1]])){
-    gene_names<<-append(gene_names,NA)
-    uniprot_ids<<-append(uniprot_ids,NA)
-  }
-  else{
-    gene_names<<-append(gene_names,list(informations['external_gene_name'][[1]]))
-    uniprot_ids<<-append(uniprot_ids,list(informations['uniprotswissprot'][[1]]))
-  }
-}
-
-#define function for querying Ensembl
-ensembl_query<-function(row){
-  mouse_strain<-row['organism_name']
-  ensembl<-ensembl_db[mouse_strain][[1]]
-  chr_name<-row['g_id']
-  numt_start<-as.numeric(row['g_start'])
-  numt_end<-numt_start+as.numeric(row['g_length'])
-  
-  out<-tryCatch(
-    {
-      
-      informations<-getBM(attributes=c('external_gene_name','uniprotswissprot'),
-                          filters=c('chromosome_name','start','end'),#all available listFilters(ensembl)
-                          values=list(chromosome=chr_name,start=numt_start,end=numt_end),#all available listAttributes(ensembl)
-                          mart=ensembl)
-      if (is.logical(informations[[1]])){
-        gene_names<<-append(gene_names,NA)
-        uniprot_ids<<-append(uniprot_ids,NA)
-      }
-      else{
-        gene_names<<-append(gene_names,list(informations['external_gene_name'][[1]]))
-        uniprot_ids<<-append(uniprot_ids,list(informations['uniprotswissprot'][[1]]))
-      }
-      
-    },
-    
-    error=function(e){
-      gene_names<<-append(gene_names,list(NA))
-      uniprot_ids<<-append(uniprot_ids,list(NA))
-    }
-  )
-  
-}
-
-gene_names<-c()
-uniprot_ids<-c()
-proba<-apply(data[1:5,],1,ensembl_query)
-gene_names
-uniprot_ids
+genes<-sapply(genomic_regions,ensembl_query)
+genes<-unlist(genes)
+df<-stack(genes)
