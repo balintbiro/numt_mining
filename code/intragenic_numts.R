de87e0702ca17500b2d5e83bba1e80657132a193
Balint Biro <biro.balint.szie@gmail.com>
2022-01-13 12:57:35 +0100

    biomaRt for every mice strains

--- code/intragenic_numts.R
+++ code/intragenic_numts.R
@@ -1,14 +1,20 @@
 
 #documentation -->https://bioconductor.org/packages/release/bioc/vignettes/biomaRt/inst/doc/accessing_ensembl.html
+#install.packages('BiocManager',quietly=TRUE)
+BiocManager::install('biomaRt')
 #load the package
 library(biomaRt)
 
+current_strain <- 'mmwsbeij'
+
 ensembl_db<-useEnsembl(biomart='mouse_strains',#select the desired database, all available listEnsembl()
-                    dataset='mmaj_gene_ensembl',#select the desired dataset, all available listDatasets(ensembl)
-                    mirror='useast')#select the mirror, options -->useast,uswest,asia,www
+                    dataset=sprintf('%s_gene_ensembl',current_strain),#select the desired dataset, all available listDatasets(ensembl)
+                    mirror='uswest')#select the mirror, options -->useast,uswest,asia,www
 
-genomic_regions<-scan('Z:/balint/numt/mice_strains_numt/genomic_regions/mmaj_gene_ensembl.txt',character())
+#get the strain specific genomic regions
+genomic_regions<-scan(sprintf('/Volumes/motilin/balint/numt/mice_strains_numt/genomic_regions/%s_gene_ensembl.txt',current_strain),character())
 
+#function for querying Ensembl
 ensembl_query<-function(genomic_region){
   ensembl<-getBM(attributes=c('external_gene_name'),
                  filters=c('chromosomal_region'),#all available listFilters(ensembl)
@@ -19,4 +25,10 @@ ensembl_query<-function(genomic_region){
 
 genes<-sapply(genomic_regions,ensembl_query)
 genes<-unlist(genes)
+
+#create dataframe from stacked named lists
 df<-stack(genes)
+
+#write output csv
+write.csv(df,sprintf('/Volumes/motilin/balint/numt/mice_strains_numt/gene_ids/%s.csv',current_strain))
+
