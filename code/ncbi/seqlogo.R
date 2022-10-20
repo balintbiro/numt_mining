0b03126284054ee8aa38fbb85f673900cd4a6d58
Balint Biro <biro.balint.szie@gmail.com>
2022-10-20 13:27:59 +0200

    sequence logo functions

--- code/ncbi/seqlogo.R
+++ code/ncbi/seqlogo.R
@@ -6,18 +6,40 @@ library(ggseqlogo)
 library(ggplot2)
 library(seqinr)
 
+#set working directory
 setwd('Documents/Projects/numt_mining/code/ncbi/')
 
-system(
-  command='ls ../'
+#read inputs
+seqlogo_input <- read.csv(
+  file='../../data/seqlogo_inputs.csv'
 )
 
-sequences <- read.fasta(
-  file='../../data/aligned_seqlogos.fa',
-  seqonly=TRUE
-)
-ggseqlogo(
-  unlist(sequences),
-  seq_type='dna',
-  method='bits'
+#check if there is a directory for the seqlogos
+if (file.exists('../../results/seqlogos/')==FALSE){
+  dir.create('../../results/seqlogos/')
+}
+
+#function for visualizing
+order_seqlogo_vis <- function(order){
+  fil <- seqlogo_input[,'order']==order
+  seqlogo <- ggseqlogo(
+    sequences[fil,]$seq,
+    seq_type='dna',
+    method='bits'
+  )+xlab('Positions')+
+    theme(axis.title.x=element_text(size=30),
+          axis.title.y=element_text(size=30))
+  ggsave(
+    f('../../results/seqlogos/%s.png',order),
+    plot=seqlogo,
+    bg='white',
+    dpi=300
+  )
+}
+
+order_seqlogo_vis('Rodentia')
+
+lapply(
+  c(unique(seqlogo_input['order'])),
+  order_seqlogo_vis
 )
