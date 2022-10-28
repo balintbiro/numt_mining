3d55ee5ce8c550cd19c12fa5a9d43a0bf5fc15f1
Balint Biro <biro.balint.szie@gmail.com>
2022-10-28 11:02:31 +0200

    numt prediction with random forest

---
+++ code/ncbi/numt_prediction.py
@@ -0,0 +1,37 @@
+#import dependencies
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report,roc_curve
+
+#import features
+features=pd.read_csv('../data/ml_features.csv')
+
+#clear features df
+features=features.dropna()
+
+#separate labels
+X=features.loc[:,features.columns!='label']
+y=features['label']
+
+#split training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+#initialize model
+model = RandomForestClassifier()
+
+#train model
+model.fit(X_train,y_train)
+
+#prediction on test set
+y_pred_test=model.predict(X_test)
+
+print(f'Accuracy score is {accuracy_score(y_test, y_pred_test)}!')
+
+#dataprep for ROC curve
+fpr, tpr, _ = roc_curve(y_test,  y_pred_test)
+fig,axs=plt.subplots(1,1)
+axs.plot(fpr,tpr)
+plt.savefig('../results/numt_rf_roc.png',dpi=200)
