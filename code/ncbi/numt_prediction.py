ebb897b126783fd92ce35d5b6227c4364ebbbce7
Balint Biro <biro.balint.szie@gmail.com>
2022-11-03 17:54:49 +0100

    working cross validation

--- code/ncbi/numt_prediction.py
+++ code/ncbi/numt_prediction.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split,GridSearchCV
-from sklearn.metrics import accuracy_score,confusion_matrix,classification_report,roc_curve,auc
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report,roc_curve,auc,RocCurveDisplay
 
 #import features
 features=pd.read_csv('../data/ml_features.csv')
@@ -58,3 +58,59 @@ rfc.fit(X_train,y_train)
 #transform the reults and save them
 #gsCV_results=pd.DataFrame.from_dict(grid_search.cv_results_)
 #gsCV_results.to_csv('../results/gsCV_results.csv')
+cv = StratifiedKFold(n_splits=10)
+classifier=RandomForestClassifier(random_state=1)
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+fig, ax = plt.subplots()
+for i, (train, test) in enumerate(cv.split(X, y)):
+    classifier.fit(X.iloc[train], y.iloc[train])
+    viz = RocCurveDisplay.from_estimator(
+        classifier,
+        X.iloc[test],
+        y.iloc[test],
+        name="ROC fold {}".format(i),
+        alpha=0.3,
+        lw=1,
+        ax=ax,
+    )
+    interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+    interp_tpr[0] = 0.0
+    tprs.append(interp_tpr)
+    aucs.append(viz.roc_auc)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+ax.plot(
+    mean_fpr,
+    mean_tpr,
+    '-',
+    color="b",
+    label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc),
+    lw=2,
+    alpha=0.8,
+)
+
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+ax.fill_between(
+    mean_fpr,
+    tprs_lower,
+    tprs_upper,
+    color="grey",
+    alpha=0.2,
+    label=r"$\pm$ 1 std. dev.",
+)
+
+ax.set(
+    xlim=[-0.05, 1.05],
+    ylim=[-0.05, 1.05]
+)
+
+ax.legend(loc="lower right")
