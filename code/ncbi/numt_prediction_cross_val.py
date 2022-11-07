b5ac30e60daf8db075a51bf9ab744c2fdc50abc4
Balint Biro <biro.balint.szie@gmail.com>
2022-11-07 15:59:32 +0100

    title formatting

--- code/ncbi/numt_prediction_cross_val.py
+++ code/ncbi/numt_prediction_cross_val.py
@@ -10,18 +10,25 @@ from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.metrics import accuracy_score,confusion_matrix,classification_report,roc_curve,auc,RocCurveDisplay
 
 #import features
-features=pd.read_csv('../data/ml_features.csv')
+data=pd.read_csv('../data/ml_features.csv')
 
 #clear features df
-features=features.dropna()
+data=data.dropna()
 
 #filter df
-fil=features.apply(lambda row:(row['upstream_size']>4900) and (row['downstream_size']>4900),axis=1)
-features=features[fil]
-features=features[['GC', 'upstream_GC', 'downstream_GC', 'uSW_mean', 'uSW_median',
+fil=data.apply(lambda row:(row['upstream_size']>4900) and (row['downstream_size']>4900),axis=1)
+data=data[fil]
+features=data[['GC', 'upstream_GC', 'downstream_GC', 'uSW_mean', 'uSW_median',
        'uRMs_count', 'uRMs_lengths', 'dSW_mean', 'dSW_median', 'dRMs_count',
        'dRMs_lengths', 'rel_start', 'entropy', 'upstream_entropy',
        'downstream_entropy', 'label','u_TmGC', 'u_TmNN', 'u_TmW', 'TmGC', 'TmNN', 'TmW', 'd_TmGC', 'd_TmNN',
+       'd_TmW']]
+
+#just flanking features
+features=data[['uSW_mean', 'uSW_median',
+       'uRMs_count', 'uRMs_lengths', 'dSW_mean', 'dSW_median', 'dRMs_count',
+       'dRMs_lengths', 'rel_start', 'upstream_entropy',
+       'downstream_entropy', 'label','u_TmGC', 'u_TmNN', 'u_TmW', 'd_TmGC', 'd_TmNN',
        'd_TmW']]
 
 #avoid imbalance-sample randomly
@@ -31,18 +38,13 @@ features=pd.concat([
     ])
 
 #separate labels
-X=features.loc[:,features.columns!='label']
-y=features['label']
-
-#split training and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+X,y=features.loc[:,features.columns!='label'],features['label']
 
 cv = StratifiedKFold(n_splits=10)
-classifier=RandomForestClassifier(random_state=1,n_estimators=5,max_depth=3)
+classifier=RandomForestClassifier(random_state=1,n_estimators=20,max_depth=5)
 
 #true positives and aucss
-tprs = []
-aucs = []
+tprs,aucs = [],[]
 mean_fpr = np.linspace(0, 1, 100)
 
 fig, ax = plt.subplots()
@@ -52,8 +54,8 @@ for i, (train, test) in enumerate(cv.split(X, y)):
         classifier,
         X.iloc[test],
         y.iloc[test],
-        name="ROC fold {}".format(i),
-        alpha=0.3,
+        name=f"ROC fold {i}",
+        alpha=0.2,
         lw=1,
         ax=ax,
     )
@@ -73,9 +75,10 @@ ax.plot(
     color="b",
     label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc),
     lw=2,
-    alpha=0.8,
+    alpha=1,
 )
 
+#visualize std
 std_tpr = np.std(tprs, axis=0)
 tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
 tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
@@ -105,8 +108,10 @@ plt.savefig('../results/cvrocs.png',dpi=200)
 
 #export feature importances
 feature_importances=pd.Series(classifier.feature_importances_)
-feature_importances.index=X_train.columns
+feature_importances.index=X.columns
+feature_importances.to_csv('../data/feature_importances.csv')
 
+#visualize feature importances
 fig, axs = plt.subplots()
 axs.barh(feature_importances.index,feature_importances)
 plt.xlabel('Feature importance')
